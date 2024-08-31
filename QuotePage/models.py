dfacbb7956e30bbbc0eb63309634225a0202bd8c
Sandipan Adhikary <87486032+Pied07@users.noreply.github.com>
2024-08-31 16:14:22 +0530

    Update models.py

--- QuotePage/models.py
+++ QuotePage/models.py
@@ -6,18 +6,6 @@ from django.conf import settings
 
 # Create your models here.
 class Quote(models.Model):
-    def get_font_choices():
-        fonts_dir = os.path.join(settings.BASE_DIR, 'QuotesApp/static/fonts')
-        font_files = os.listdir(fonts_dir)
-        font_choices=[]
-
-        for font_file in font_files:
-            if font_file.endswith(('.ttf')):
-                font_name = os.path.splitext(font_file)[0]
-                font_choices.append((font_name,font_name))
-        return font_choices
-
-
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     title = models.CharField(max_length=200)
@@ -28,8 +16,6 @@ class Quote(models.Model):
     caption = models.TextField(max_length=1000,null=True,blank=True)
     fontcolor = models.CharField(max_length=10,default='#000000')
     fontsize = models.IntegerField(default=20)
-    fontfamily = models.CharField(max_length=50,choices=get_font_choices(),help_text="Choose a Font for Your Quote!",default='arial')
-
 
     def __str__(self):
         return self.user.username
